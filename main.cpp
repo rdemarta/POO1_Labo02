6cb4c889ef0e74a11c5c23555454e294ccb8d4d1
Robin <r.demarta@outlook.com>
2020-03-12 16:54:05 +0100

    CMake file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+ * File     : main.cpp
+ * Authors  : Robin Demarta, Loïc Dessaules
+ * Date     : 12.03.2020
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
